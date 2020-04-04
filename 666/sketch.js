99452319b69fa934cb4d30c7a104cc6940071962
veikan <61820614+veikan@users.noreply.github.com>
2020-04-04 20:25:11 +0800

    upload data of p5js

---
+++ 666/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  var cx = map(mouaeX, 0, width, 0, 255);
+  var cy = map(mouaeY, 0, height, 0, 255);
+  var nsize = width/8+cos(mouseY*PI/180)*width/16;
+  fill(cx,cy,150,50);
+  circle(mouseX,mouseY,nsize);
+}
